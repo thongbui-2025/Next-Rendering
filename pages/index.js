84e6b19c0cfbcc496f9560e12da970ddf699ae8b
vanthong <buivanthong1704@gmail.com>
2025-08-08 00:28:51 +0700

    update Next version and UI Home commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+	return (
+		<div>
+			<h1>Home</h1>
+			<nav>
+				<ul>
+					<li>
+						<Link href="/csr">Client Side Rendering (CSR)</Link>
+					</li>
+					<li>
+						<Link href="/ssr">Server Side Rendering (SSR)</Link>
+					</li>
+					<li>
+						<Link href="/ssg">Static Site Generation (SSG)</Link>
+					</li>
+					<li>
+						<Link href="/isr">
+							Incremental Static Regeneration (ISR)
+						</Link>
+					</li>
+					<li>
+						<Link href="/isr-on-demand">ISR On-Demand</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
 
 // this page is unused
